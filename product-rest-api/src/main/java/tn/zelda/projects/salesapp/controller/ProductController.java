094406e982e7fd03fac3e08e32e97c9e62c795dc
Oussema TOUKEBRI <oussema.toukebri@gmail.com>
2017-01-31 07:50:20 +0100

    feat(product) init module implementation

---
+++ product-rest-api/src/main/java/tn/zelda/projects/salesapp/controller/ProductController.java
@@ -0,0 +1,17 @@
+package tn.zelda.projects.salesapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+/**
+ * Created by zelda on 31/01/17.
+ */
+
+@Controller
+public class ProductController {
+
+    public List<ProductController> getProducts(){
+        return null;
+    }
+}
