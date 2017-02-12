4b511a5030b7aecf436d7a2ae985ddfa9322752c
Oussema TOUKEBRI <oussema.toukebri@gmail.com>
2017-02-12 18:48:31 +0100

    fix (client-rest-api) support of spring cloud eureka client

--- client-rest-api/src/main/java/tn/zelda/projects/salesapp/ProductsRestApiApplication.java
+++ client-rest-api/src/main/java/tn/zelda/projects/salesapp/ProductsRestApiApplication.java
@@ -2,11 +2,14 @@ package tn.zelda.projects.salesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class ProductsRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductsRestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsRestApiApplication.class, args);
+    }
 }
