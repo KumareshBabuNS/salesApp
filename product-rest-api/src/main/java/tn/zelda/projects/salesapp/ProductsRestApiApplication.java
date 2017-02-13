9d69f1d907ed467a87332e67f52c0c5e390c12b8
Oussema TOUKEBRI <oussema.toukebri@gmail.com>
2017-02-13 21:34:18 +0100

    fix(pom) add ribbon module and some enhancement

--- product-rest-api/src/main/java/tn/zelda/projects/salesapp/ProductsRestApiApplication.java
+++ product-rest-api/src/main/java/tn/zelda/projects/salesapp/ProductsRestApiApplication.java
@@ -2,8 +2,12 @@ package tn.zelda.projects.salesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableCircuitBreaker
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductsRestApiApplication {
 
 	public static void main(String[] args) {
